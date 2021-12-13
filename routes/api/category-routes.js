1f48e8b755a805d19a01175faeb47240cc0f3c53
bchen41 <bettychen41@outlook.com>
2021-12-12 22:18:50 -0500

    added new category POST handler

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -35,14 +35,26 @@ router.get("/:id", async (req, res) => {
       res.status(404).json({ message: "No category found with this id!" });
       return;
     }
-    res.status(200).json(categoryData);
+    res.status(200).json({
+      message: `${req.method} data retrieved!`,
+      data: categoryData,
+    });
   } catch (err) {
     res.status(500).json(err);
   }
 });
 
-router.post("/", (req, res) => {
+router.post("/", async (req, res) => {
   // create a new category
+  try {
+    const categoryData = await Category.create(req.body);
+    res.status(200).json({
+      message: `${req.method} category added!`,
+      data: categoryData,
+    });
+  } catch (err) {
+    res.status(400).json(err);
+  }
 });
 
 router.put("/:id", (req, res) => {
