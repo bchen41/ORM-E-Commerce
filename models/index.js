a7680365f9037bfb2ba23c8a00bed38e66d042f4
bchen41 <bettychen41@outlook.com>
2021-12-14 18:41:06 -0500

    fk and onDelete in Products not Category

--- models/index.js
+++ models/index.js
@@ -7,13 +7,11 @@ const ProductTag = require("./ProductTag");
 // Products belongsTo Category
 Product.belongsTo(Category, {
   foreignKey: "category_id",
+  onDelete: "CASCADE",
 });
 
 // Categories have many Products
-Category.hasMany(Product, {
-  foreignKey: "category_id",
-  onDelete: "CASCADE",
-});
+Category.hasMany(Product);
 
 // Products belongToMany Tags (through ProductTag)
 Product.belongsToMany(Tag, { through: ProductTag });
