1281db59e89c318975bbfacdcb21ff6f666163e8
bchen41 <bettychen41@outlook.com>
2021-12-15 22:19:13 -0500

    removed comments

--- models/ProductTag.js
+++ models/ProductTag.js
@@ -1,13 +1,11 @@
 const { Model, DataTypes } = require("sequelize");
 
 const sequelize = require("../config/connection");
-const Product = require("./Product");
 
 class ProductTag extends Model {}
 
 ProductTag.init(
   {
-    // define columns
     id: {
       type: DataTypes.INTEGER,
       allowNull: false,
